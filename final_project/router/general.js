cefa18f61b11c581ee5bf9d53d4f700c88c17dff
Igor Savinkin <igor.savinkin@gmail.com>
2025-06-02 22:19:41 +0300

    filter by author, title, isbn thru asynch Promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-var filterbyISBN = function filterBooksByISBN(booksObj, targetISBN) {
+function filterBooksByISBN(booksObj, targetISBN) {
     return Object.values(booksObj).filter(book => book.isbn === targetISBN);
   }
 function filterBooksByAuthor(booksObj, targetAuthor) {
@@ -27,6 +27,7 @@ public_users.post("/register", (req,res) => {
 
 public_users.get('/',function (req, res) {
 	let myPromise = new Promise((resolve,reject) => {   
+		res.set('Content-Type', 'application/json');
 		res.send(JSON.stringify({books}, null, 4));
 	})
 	.then(() => { 
@@ -35,35 +36,55 @@ public_users.get('/',function (req, res) {
 	.catch((err)=> { res.status(400).send("An error in getting books: "+ err) });	 
 });
 
-// Get book details based on ISBN
+// Get book details based on ISBN thru Promise
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  let filteredBooks = filterBooksByISBN(books, req.params.isbn);
-  if (filteredBooks.length > 0 ) {
-    return res.send(JSON.stringify({filteredBooks}, null, 4));
-  } else {
-    return res.send("No books in store for isbn: " + req.params.isbn )
-  }
+	let myPromise = new Promise((resolve,reject) => {   
+		let filteredBooks = filterBooksByISBN(books, req.params.isbn);
+		if (filteredBooks.length > 0 ) {
+			res.set('Content-Type', 'application/json');
+			res.send(JSON.stringify({filteredBooks}, null, 4));
+		} else {
+			res.send("No books in store for isbn: " + req.params.isbn )
+		}
+	})
+	.then(() => { 
+	    console.log("Books are loaded."); 
+	})
+	.catch((err)=> { res.status(400).send("An error in getting books: "+ err) });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    let filteredBooks = filterBooksByAuthor(books, req.params.author);
-    if (filteredBooks.length > 0 ) {
-        return res.send(JSON.stringify({filteredBooks}, null, 4));
-    } else {
-        return res.send("No books in store for isbn: " + req.params.isbn )
-    }
+	let myPromise = new Promise((resolve,reject) => {   
+		let filteredBooks = filterBooksByAuthor(books, req.params.author);
+		if (filteredBooks.length > 0 ) {
+			res.set('Content-Type', 'application/json');
+			res.send(JSON.stringify({filteredBooks}, null, 4));
+		} else {
+			res.send("No books in store for the author: " + req.params.author )
+		}
+	})
+	.then(() => { 
+	    console.log("Books are loaded."); 
+	})
+	.catch((err)=> { res.status(400).send("An error in getting books: "+ err) });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {    
-    let filteredBooks = filterBooksByTitle(books, req.params.title);
-    if (filteredBooks.length > 0 ) {
-        return res.send(JSON.stringify({filteredBooks}, null, 4));
-    } else {
-        return res.send("No books in store for isbn: " + req.params.title )
-    }
+public_users.get('/title/:title',function (req, res) {  
+	let myPromise = new Promise((resolve,reject) => {   
+		let filteredBooks = filterBooksByTitle(books, req.params.title);
+		if (filteredBooks.length > 0 ) {
+			res.set('Content-Type', 'application/json');
+			res.send(JSON.stringify({filteredBooks}, null, 4));
+		} else {
+			res.send("No books in store with the title: " + req.params.title )
+		}
+	})
+	.then(() => { 
+	    console.log("Books are loaded."); 
+	})
+	.catch((err)=> { res.status(400).send("An error in getting books: "+ err) });
 });
 
 //  Get book review
@@ -77,5 +98,4 @@ public_users.get('/review/:isbn',function (req, res) {
     }
 });
 
-module.exports.general = public_users;
-module.exports.filterbyISBN = filterbyISBN;
+module.exports.general = public_users; 
