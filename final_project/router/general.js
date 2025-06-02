c09c4235160e2e8e6d6fa6ce589dd0864ac42cb2
Igor Savinkin <igor.savinkin@gmail.com>
2025-06-02 06:56:44 -0400

    filter by author, title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,6 +7,12 @@ const public_users = express.Router();
 function filterBooksByISBN(booksObj, targetISBN) {
     return Object.values(booksObj).filter(book => book.isbn === targetISBN);
   }
+function filterBooksByAuthor(booksObj, targetAuthor) {
+    return Object.values(booksObj).filter(book => book.author === targetAuthor);
+  }
+function filterBooksByTitle(booksObj, targetTitle) {
+    return Object.values(booksObj).filter(book => book.title === targetTitle);
+  }
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -22,7 +28,7 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  const filteredBooks = filterBooksByISBN(books, req.params.isbn);
+  let filteredBooks = filterBooksByISBN(books, req.params.isbn);
   if (filteredBooks.length > 0 ) {
     return res.send(JSON.stringify({filteredBooks}, null, 4));
   } else {
@@ -32,14 +38,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let filteredBooks = filterBooksByAuthor(books, req.params.author);
+    if (filteredBooks.length > 0 ) {
+        return res.send(JSON.stringify({filteredBooks}, null, 4));
+    } else {
+        return res.send("No books in store for isbn: " + req.params.isbn )
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title',function (req, res) {    
+    let filteredBooks = filterBooksByTitle(books, req.params.title);
+    if (filteredBooks.length > 0 ) {
+        return res.send(JSON.stringify({filteredBooks}, null, 4));
+    } else {
+        return res.send("No books in store for isbn: " + req.params.title )
+    }
 });
 
 //  Get book review
