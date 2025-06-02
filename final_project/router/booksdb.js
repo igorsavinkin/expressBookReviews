b68d11f6b8431e14c2818e2418e285fc05a6118c
Igor Savinkin <igor.savinkin@gmail.com>
2025-06-02 05:15:45 -0400

    filter by ISBN

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,64 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+    1: {
+        "author": "Chinua Achebe",
+        "title": "Things Fall Apart",
+        "reviews": {},
+        "isbn": "978-0385474542"
+    },
+    2: {
+        "author": "Hans Christian Andersen",
+        "title": "Fairy tales",
+        "reviews": {},
+        "isbn": "978-0141322433"   
+    },
+    3: {
+        "author": "Dante Alighieri",
+        "title": "The Divine Comedy",
+        "reviews": {},
+        "isbn": "978-0140448955" 
+    },
+    4: {
+        "author": "Unknown",
+        "title": "The Epic Of Gilgamesh",
+        "reviews": {},
+        "isbn": "978-0140449198"  
+    },
+    5: {
+        "author": "Unknown",
+        "title": "The Book Of Job",
+        "reviews": {},
+        "isbn": "978-0140449266"   
+    },
+    6: {
+        "author": "Unknown",
+        "title": "One Thousand and One Nights",
+        "reviews": {},
+        "isbn": "978-0140449266"  
+    },
+    7: {
+        "author": "Unknown",
+        "title": "Njál's Saga",
+        "reviews": {},
+        "isbn": "978-0140447694"  
+    },
+    8: {
+        "author": "Jane Austen",
+        "title": "Pride and Prejudice",
+        "reviews": {},
+        "isbn": "978-0141439518"
+    },
+    9: {
+        "author": "Honoré de Balzac",
+        "title": "Le Père Goriot",
+        "reviews": {},
+        "isbn": "978-0140449235"
+    },
+    10: {
+        "author": "Samuel Beckett",
+        "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+        "reviews": {},
+        "isbn": "978-0802141324"   
+    }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,9 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function filterBooksByISBN(booksObj, targetISBN) {
+    return Object.values(booksObj).filter(book => book.isbn === targetISBN);
+  }
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -12,14 +15,19 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  
-  return res.status(200).json(JSON.stringify( books));
+  res.set('Content-Type', 'application/json');
+  return res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const filteredBooks = filterBooksByISBN(books, req.params.isbn);
+  if (filteredBooks.length > 0 ) {
+    return res.send(JSON.stringify({filteredBooks}, null, 4));
+  } else {
+    return res.send("No books in store for isbn: " + req.params.isbn )
+  }
  });
   
 // Get book details based on author
